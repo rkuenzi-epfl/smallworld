ca8bdf68e97f593fc72decd00a5be317cf685edf
rkuenzi <romain.kuenzi@epfl.ch>
2021-06-17 00:57:13 +0200

    Update config files and install script

--- install.sh
+++ install.sh
@@ -13,6 +13,12 @@ cp ./exampleConfigurations/wpa_noAP.conf /etc/smallworld/
 cp ./smallworld /usr/sbin/
 cp ./smallworldForwardEvent /usr/sbin/
 
+# Copy AP and DHCP conf files
+cp ./exampleConfigurations/hostapd.conf /etc/hostapd/hostapd.conf
+cp ./exampleConfigurations/hostapd.psk /etc/hostapd/hostapd.psk
+cp ./exampleConfigurations/dnsmasq.conf /etc/dnsmasq.conf
+cp ./exampleConfigurations/dhcpcd.conf /etc/dhcpcd.conf
+
 # Copy service file
 cp ./smallworld.service /lib/systemd/system/
 
